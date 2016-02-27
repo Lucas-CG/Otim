6b499cad74d3c147e379c96d1763005e45550b85
patykov <patriciakovaleski@gmail.com>
2016-02-27 18:38:51 -0300

    Apenas escrevendo as funcoes a e b

---
+++ trab_OTM.py
@@ -0,0 +1,21 @@
+import sys
+import math
+import numpy  
+
+def fA (x1, x3):
+	return - math.exp(   -pow(x1, 2) - pow(pow(x1, 2)- x2, 2)   )
+
+def fB (x1, x2):
+	print (  math.sqrt(  pow(x1,2) + pow(  (pow(x1,2) - x2), 2  )  )    )
+
+def grad_fA(x1, x2):
+
+
+
+
+x1 = 2
+x2 = 5
+
+fB(x1, x2)
+
+
