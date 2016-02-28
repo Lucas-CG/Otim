fa7741c9278dc907fc037165ab1a487b873814c7
Lucas-CG <lucas.gomes@poli.ufrj.br>
2016-02-27 22:25:46 -0300

    pequena correção

--- trab_OTM.py
+++ trab_OTM.py
@@ -9,14 +9,16 @@ def fB (x1, x2):
 	return  math.sqrt( pow(x1,2) + pow(  (pow(x1,2) - x2), 2) )
 
 def grad_fA(x1, x2):
-	deriv_x1 = ( 2 * x1 + 4 * pow(x1, 3) - 4 * x1 * x2 ) * fA(x1, x2)
-	deriv_x2 = -2 * ( pow(x1, 2) - x2 ) * fA(x1, x2)
+	deriv_x1 = - ( 2 * x1 + 4 * pow(x1, 3) - 4 * x1 * x2 ) * fA(x1, x2)
+	deriv_x2 = 2 * ( pow(x1, 2) - x2 ) * fA(x1, x2)
 	return [deriv_x1, deriv_x2]
 
 def grad_fB(x1, x2):
 	deriv_x1 = (x1 + 2 * pow(x1, 3) - 2 * x1 * x2) / fB(x1, x2)
 	deriv_x2 = ( x2 - pow(x1, 2) ) / fB(x1, x2)
 	return [deriv_x1, deriv_x2]
+
+def armijo()
 
 #Argumentos: funcao, minimizador, ponto a ser verificado, direcao de descida, funcao gradiente
 #def gradient (func, minimazer, x, d, grad):
