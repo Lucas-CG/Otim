e32e2b3a4bf539e9e9a0b897597dc07bbb9bd42e
Lucas-CG <lucas.gomes@poli.ufrj.br>
2016-02-27 21:54:53 -0300

    Derivadas adicionadas

--- trab_OTM.py
+++ trab_OTM.py
@@ -1,21 +1,21 @@
 import sys
 import math
-import numpy  
+import numpy
 
-def fA (x1, x3):
+def fA (x1, x2):
 	return - math.exp( -pow(x1, 2) - pow(pow(x1, 2)- x2, 2) )
 
 def fB (x1, x2):
 	return  math.sqrt( pow(x1,2) + pow(  (pow(x1,2) - x2), 2) )
 
 def grad_fA(x1, x2):
-	deriv_x1 =
-	deriv_x2 =
+	deriv_x1 = ( 2 * x1 + 4 * pow(x1, 3) - 4 * x1 * x2 ) * fA(x1, x2)
+	deriv_x2 = -2 * ( pow(x1, 2) - x2 ) * fA(x1, x2)
 	return [deriv_x1, deriv_x2]
 
 def grad_fB(x1, x2):
-	deriv_x1 =
-	deriv_x2 =
+	deriv_x1 = (x1 + 2 * pow(x1, 3) - 2 * x1 * x2) / fB(x1, x2)
+	deriv_x2 = ( x2 - pow(x1, 2) ) / fB(x1, x2)
 	return [deriv_x1, deriv_x2]
 
 #Argumentos: funcao, minimizador, ponto a ser verificado, direcao de descida, funcao gradiente
@@ -33,5 +33,3 @@ x1 = 2
 x2 = 5
 
 fB(x1, x2)
-
-
