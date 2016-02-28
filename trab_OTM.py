7d95d1ebc7491fbcd28d535cc947c43cf9b97d2c
Lucas-CG <lucas.gomes@poli.ufrj.br>
2016-02-27 23:18:04 -0300

    + busca de armijo

--- trab_OTM.py
+++ trab_OTM.py
@@ -18,7 +18,21 @@ def grad_fB(x1, x2):
 	deriv_x2 = ( x2 - pow(x1, 2) ) / fB(x1, x2)
 	return [deriv_x1, deriv_x2]
 
-def armijo()
+#busca de armijo: arumentos: função, ponto (x1, x2), direção (dx1, dx2), taxa de atualização de t (gamma)
+# e eta (multiplicador para a direção de descida - ?)
+def armijo(func, x1, x2, dx1, dx2, gamma, eta): #retorno: tamanho do passo
+
+	t = 1
+
+	if func == "a":
+		while fA(x1 + t * dx1, x2 + t * dx2) > fA(x1, x2) + eta * t * (grad_fA(x1, x2)[0] * dx1 + grad_fA(x1, x2)[1] * dx2):
+			t = gamma * t
+
+	if func == "b":
+		while fB(x1 + t * dx1, x2 + t * dx2) > fB(x1, x2) + eta * t * (grad_fB(x1, x2)[0] * dx1 + grad_fB(x1, x2)[1] * dx2):
+			t = gamma * t
+
+	return t
 
 #Argumentos: funcao, minimizador, ponto a ser verificado, direcao de descida, funcao gradiente
 #def gradient (func, minimazer, x, d, grad):
