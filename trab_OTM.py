fe4000f01d3bd413a3137a3bd2f0293efd3a8333
patykov <patriciakovaleski@gmail.com>
2016-02-29 08:34:19 -0300

    primeira tentativa de escrever os minimizadores

--- trab_OTM.py
+++ trab_OTM.py
@@ -18,17 +18,57 @@ def grad_fB(x1, x2):
 	deriv_x2 = ( x2 - pow(x1, 2) ) / fB(x1, x2)
 	return [deriv_x1, deriv_x2]
 
-def armijo()
 
-#Argumentos: funcao, minimizador, ponto a ser verificado, direcao de descida, funcao gradiente
-#def gradient (func, minimazer, x, d, grad):
+def armijo():
+#Argumentos: funcao, ponto a ser verificado, direcao de descida, funcao gradiente
+#def gradient (func, x, d, grad):
 #	t = 1
 #	while func(x + t*d) > ( func(x) + NI*t*grad(x)*d ):
 #		t = LAMBA*t
-#
+#	return t
 #O que é NI e LAMBDA???
 
+def gradiente(func, grad_func, x):
+#	k = 0
+#	x_ant = x
+#	while ((grad_func(x)!=0) || (x == x_ant)):
+#		d = -grad_func(x)
+#		t = armijo(func, x, d, grad_func)
+#		x_prox = x + t*d
+#		k = k+1
+#		x_ant = x
+#		x = x_prox
+#	return x
 
+
+def newton(func, grad_func, x):
+#	k = 0
+#	x_ant = x
+#	while ( (grad_func != 0) || (x_ant == x) ):
+#		d = -(inv_hess(x)) * grad_func(x)
+#		t = armijo(func, x, d, grad_func)
+#		x_prox = x + t*d
+#		k = k + 1
+#		x_ant = x
+#		x = x_prox
+#	return x
+
+
+def quaseNewton(func, grad_func, x):
+#	k = 0
+#	x_ant = x
+#	Hk = DEFINIR
+#	while ( (grad_func(x)!=0) || (x_ant==x) ):
+#		d = -(Hk)*grad_func(x)
+#		t = armijo(func, x, d, grad_func)
+#		x_prox = x + t*d
+#		Hk_prox = H_k(x_prox, Hk)
+#		k = k + 1
+#		x_ant = x
+#		x = x_prox
+
+def Hk_prox(x, Hk):
+	
 
 
 x1 = 2
